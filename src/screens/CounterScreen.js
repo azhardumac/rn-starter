56e8077b2f431ad01928b5f0adc4e9dd8605f440
azhardumac <azhardumac@gmail.com>
2023-10-23 22:55:33 +0530

    updated with reducer instead of usestate

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,9 +1,24 @@
-import React, {useEffect, useState} from 'react';
+// import React, {useEffect, useState} from 'react';
+import React, {useReducer} from 'react';
 import { View, Text, StyleSheet, Button } from 'react-native';
+
+
+const reducer = (state,action)=>{
+
+    switch(action.type){
+        case 'increase' :
+            return{...state, count: state.count + action.payload};
+        case 'decrease' :
+            return{...state, count: state.count - action.payload};
+        default:
+        return state;
+    }
+
+};
 
 const CounterScreen = () => {
 
-    const [counter, setCounter] = useState(0);
+    const [state, dispatch] = useReducer(reducer,{count:0 });
 
 
 
@@ -11,16 +26,17 @@ const CounterScreen = () => {
         <Button 
             title="Increase"
             onPress={()=>{
-                setCounter (counter + 1);
+                dispatch({type:'increase',payload : 1})
+                console.log(state);
             }}
         />
         <Button 
             title="Decrease"
             onPress={()=>{
-                setCounter (counter - 1);
+                dispatch({type:'decrease',payload : 1})
             }}
         />
-        <Text>Current Count:{counter}</Text>
+        <Text>Current Count:{state.count}</Text>
     </View>
 
 };
@@ -31,3 +47,31 @@ const styles = StyleSheet.create({
 });
 
 export default CounterScreen;
+
+
+
+
+
+// const CounterScreen = () => {
+
+//     const [counter, setCounter] = useState(0);
+
+
+
+//     return <View>
+//         <Button 
+//             title="Increase"
+//             onPress={()=>{
+//                 setCounter (counter + 1);
+//             }}
+//         />
+//         <Button 
+//             title="Decrease"
+//             onPress={()=>{
+//                 setCounter (counter - 1);
+//             }}
+//         />
+//         <Text>Current Count:{counter}</Text>
+//     </View>
+
+// };
